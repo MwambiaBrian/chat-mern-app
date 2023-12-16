f85e59a8783b0927d69090dbfc7bcaaffd949229
MwambiaBrian <brianmwambia4@gmail.com>
2023-12-16 08:57:53 +0300

    set minisize chunks

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust to your preferred limit
+  },
   plugins: [react()],
-})
+});
